2379035e0d318f9e4c5a38c2f7707c383055ff06
one-think-0116 <80469201+one-think-0116@users.noreply.github.com>
2011-12-06 13:11:11 -0300

    Cleanup destdir so old documents don't appear in the d.dp.com site and tarball after docu refactorings.

--- django_website/urls/www.py
+++ django_website/urls/www.py
@@ -1,20 +1,18 @@
 from __future__ import absolute_import
 
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include, url
-from django.contrib import admin
+from django.conf.urls.defaults import *
+from django.contrib import admin; admin.autodiscover()
 from django.contrib.comments.feeds import LatestCommentFeed
 from django.contrib.comments.models import Comment
 from django.contrib.flatpages.views import flatpage
 from django.contrib.sitemaps import views as sitemap_views
 from django.views.decorators.cache import cache_page
 from django.views.generic.simple import redirect_to
-from ..accounts import views as account_views
 from ..aggregator.feeds import CommunityAggregatorFeed, CommunityAggregatorFirehoseFeed
+from ..aggregator.models import FeedItem
 from ..blog.feeds import WeblogEntryFeed
 from ..sitemaps import FlatPageSitemap, WeblogSitemap
-
-admin.autodiscover()
 
 comments_info_dict = {
     'queryset': Comment.objects.filter(is_public=True).order_by('-submit_date'),
@@ -36,10 +34,6 @@ urlpatterns = patterns('',
     url(r'^contact/', include('django_website.contact.urls')),
     url(r'^r/', include('django.conf.urls.shortcut')),
 
-    # There's no school like the old school.
-    url(r'^~(?P<username>[\w-]+)/$', account_views.user_profile, name='user_profile'),
-
-    # Feeds
     url(r'^rss/weblog/$', WeblogEntryFeed(), name='weblog-feed'),
     url(r'^rss/comments/$', LatestCommentFeed(), name='comments-feed'),
     url(r'^rss/community/$', redirect_to, {'url': '/rss/community/blogs/'}),
@@ -66,3 +60,10 @@ if not settings.PRODUCTION:
             "document_root": settings.MEDIA_ROOT,
         }),
     )
+
+urlpatterns += patterns('',
+# flatpages need to be last b/c they match anything
+    (r'', include('django.contrib.flatpages.urls')),
+)
+
+admin.autodiscover()
