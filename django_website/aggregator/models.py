31648c3037076f31790a8be134fd2621ce923762
one-think-0116 <80469201+one-think-0116@users.noreply.github.com>
2012-02-03 00:22:30 -0800

    Adding approval_status to Feed objects (with accompanying admin).

--- django_website/aggregator/models.py
+++ django_website/aggregator/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django_push.subscriber import signals as push_signals
 from django_push.subscriber.models import Subscription
+from django.conf import settings
 
 log = logging.getLogger(__name__)
 
@@ -19,61 +20,71 @@ class FeedType(models.Model):
     def items(self):
         return FeedItem.objects.filter(feed__feed_type=self)
 
+STATUS_CHOICES = (
+    ('P', 'Pending'),
+    ('D', 'Denied'),
+    ('A', 'Approved')
+)
+
+
 class Feed(models.Model):
     title = models.CharField(max_length=500)
     feed_url = models.URLField(unique=True, max_length=500)
     public_url = models.URLField(max_length=500)
     is_defunct = models.BooleanField()
+    approval_status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
     feed_type = models.ForeignKey(FeedType)
     owner = models.ForeignKey(User, blank=True, null=True, related_name='owned_feeds')
 
     def __unicode__(self):
         return self.title
-    
+
     def save(self, **kwargs):
         super(Feed, self).save(**kwargs)
-        Subscription.objects.subscribe(self.feed_url, settings.PUSH_HUB)
+        if settings.PRODUCTION and self.approval_status == STATUS_CHOICES[2][0]:
+            Subscription.objects.subscribe(self.feed_url, settings.PUSH_HUB)
 
     def delete(self, **kwargs):
         super(Feed, self).delete(**kwargs)
-        Subscription.objects.unsubscribe(self.feed_url, settings.PUSH_HUB)
+        if settings.PRODUCTION: # @@@ need to validate what pubsub stuff is actually doing here.
+            Subscription.objects.unsubscribe(self.feed_url, settings.PUSH_HUB)
 
 class FeedItemManager(models.Manager):
     def create_or_update_by_guid(self, guid, **kwargs):
         """
         Look up a FeedItem by GUID, updating it if it exists, and creating
         it if it doesn't.
-        
+
         We don't limit it by feed because an item could be in another feed if
         some feeds are themselves aggregators. That's also why we don't update
         the feed field if the feed item already exists.
-        
+
         Returns (item, created) like get_or_create().
         """
         try:
             item = self.get(guid=guid)
-        
+
         except self.model.DoesNotExist:
             # Create a new item
             log.debug('Creating entry: %s', guid)
             kwargs['guid'] = guid
             item = self.create(**kwargs)
-            
+
         else:
             log.debug('Updating entry: %s', guid)
 
             # Update an existing one.
             kwargs.pop('feed', None)
-            
+
             # Don't update the date since most feeds get this wrong.
             kwargs.pop('date_modified')
-            
+
             for k,v in kwargs.items():
                 setattr(item, k, v)
             item.save()
-            
+
         return item
-        
+
 class FeedItem(models.Model):
     feed = models.ForeignKey(Feed)
     title = models.CharField(max_length=500)
@@ -100,12 +111,15 @@ def feed_updated(sender, notification, **kwargs):
     except Feed.DoesNotExist:
         log.error("Subscription ID %s (%s) doesn't have a feed.", sender.id, sender.topic)
         return
-        
+
     for entry in notification.entries:
         title = entry.title
-        guid = entry.get("id", entry.link)
+        try:
+            guid = entry.get("id", entry.link)
+        except AttributeError:
+            log.error("Feed ID %s has an entry ('%s') without a link or guid. Skipping.", feed.id, title)
         link = entry.link or guid
-                    
+
         if hasattr(entry, "summary"):
             content = entry.summary
         elif hasattr(entry, "content"):
@@ -119,14 +133,13 @@ def feed_updated(sender, notification, **kwargs):
             date_modified = datetime.datetime(*entry.updated_parsed[:6])
         else:
             date_modified = datetime.datetime.now()
-        
+
         FeedItem.objects.create_or_update_by_guid(guid,
             feed = feed,
             title = title,
             link = link,
             summary = content,
             date_modified = date_modified
-        )    
+        )
 
 push_signals.updated.connect(feed_updated)
-    
