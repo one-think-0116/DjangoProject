f932c807225efec0b45f75be77993ea40e616ef7
one-think-0116 <80469201+one-think-0116@users.noreply.github.com>
2014-12-30 00:04:36 +0100

    Removed unneeded static template loads.

--- blog/urls.py
+++ blog/urls.py
@@ -6,23 +6,19 @@ from . import views
 urlpatterns = [
     url(
         r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[\w-]+)/$',
-        views.BlogDateDetailView.as_view(),
-        name="blog-entry"
+        views.BlogDateDetailView.as_view()
     ),
     url(
         r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',
-        views.BlogDayArchiveView.as_view(),
-        name="blog-archive-day"
+        views.BlogDayArchiveView.as_view()
     ),
     url(
         r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',
-        views.BlogMonthArchiveView.as_view(),
-        name="blog-archive-month"
+        views.BlogMonthArchiveView.as_view()
     ),
     url(
         r'^(?P<year>\d{4})/$',
-        views.BlogYearArchiveView.as_view(),
-        name="blog-archive-year"
+        views.BlogYearArchiveView.as_view()
     ),
     url(
         r'^/?$',
