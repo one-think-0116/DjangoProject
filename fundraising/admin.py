c13bdd7021d15829b8c37f8c86d438c49a610dc0
one-think-0116 <80469201+one-think-0116@users.noreply.github.com>
2015-06-04 11:24:53 +0100

    Fixed redirect url on cancel page

--- fundraising/admin.py
+++ fundraising/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from sorl.thumbnail.admin import AdminImageMixin
 
-from .models import DjangoHero, Donation, Testimonial
+from .models import Campaign, DjangoHero, Donation, Testimonial
 
 
 @admin.register(DjangoHero)
 class DjangoHeroAdmin(AdminImageMixin, admin.ModelAdmin):
-    list_filter = ['approved', 'created', 'modified',
-                   'is_visible', 'is_subscribed', 'is_amount_displayed']
+    list_filter = ['approved', 'created', 'modified', 'is_visible', 'is_subscribed']
     list_display = ['id', 'name', 'email', 'created', 'modified', 'approved']
     list_editable = ['approved']
     ordering = ['-created']
@@ -17,11 +16,19 @@ class DjangoHeroAdmin(AdminImageMixin, admin.ModelAdmin):
 class Donation(admin.ModelAdmin):
     raw_id_fields = ['donor']
     list_display = ['id', 'amount', 'donor', 'stripe_charge_id',
-                    'created', 'modified', 'campaign_name']
-    list_filter = ['campaign_name', 'created', 'modified']
+                    'created', 'modified']
+    list_filter = ['created', 'modified']
     ordering = ['-created']
 
 
 @admin.register(Testimonial)
 class Testimonial(admin.ModelAdmin):
     pass
+
+
+@admin.register(Campaign)
+class Campaign(admin.ModelAdmin):
+    list_display = ['name', 'goal', 'template', 'stretch_goal',
+                    'start_date', 'end_date', 'is_active', 'is_public']
+    list_filter = ['is_active', 'is_public']
+    prepopulated_fields = {'slug': ('name',)}
